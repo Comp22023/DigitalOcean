2dade67354cca8f138ff4a15a7d0370af6cddbe1
Comp022023 <kyurinova@inbox.ru>
2023-09-30 14:41:11 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 const url = 'https://reqres.in/api/users?page=2';
 
+let test = []
+
 document.querySelector('#get-text').onclick = async () => {
     try {
     let response = await fetch(url);
     if (response.ok) {
       let data = await response.text();
-//      console.log(data);
-      document.querySelector('#result').innerHTML = data;
+      let obj = JSON.parse(data);
+      for (let i=0; i<obj.data.length; i++) {
+        let counter = obj.data[i];
+        test.push(`id:${counter.id}, first name:${counter.first_name}, last name:${counter.last_name}, email:${counter.email}, avatar:${counter.avatar}`);
+        console.log(test);
+        document.querySelector('#result').innerHTML = test;
+      }
     }
     }
     catch (error) {
         console.log(error);
    }
 }
+
+//const json = '{ "fruit": "pineapple", "fingers": [{"id":10}] }';
+//const obj = JSON.parse(json);
+//console.log(obj.fingers);
+
+//for (var i=0; i<jsonData.counters.length; i++) {
+//    var counter = jsonData.counters[i];
+//    console.log(counter.counter_name);
+//}
