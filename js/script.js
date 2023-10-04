61c4404c6efaf293f2151c3ed3e057d77e313fb4
Comp022023 <kyurinova@inbox.ru>
2023-10-04 12:23:34 +0300

    задача с проверкой треугольника

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-const url = 'https://reqres.in/api/users?page=2';
-
-let test = []
-
-document.querySelector('#get-text').onclick = async () => {
-    try {
-    let response = await fetch(url);
-    if (response.ok) {
-      let data = await response.text();
-      let obj = JSON.parse(data);
-      for (let i=0; i<obj.data.length; i++) {
-        let counter = obj.data[i];
-        test.push(`id:${counter.id}, first name:${counter.first_name}, last name:${counter.last_name}, email:${counter.email}, avatar:${counter.avatar}`);
-        console.log(test);
-        document.querySelector('#result').innerHTML = test;
-      }
-    }
-    }
-    catch (error) {
-        console.log(error);
-   }
-}
+////const url = 'https://reqres.in/api/users?page=2';
+//
+//let test = []
+//
+//document.querySelector('#get-text').onclick = async () => {
+//    try {
+//    let response = await fetch(url);
+//    if (response.ok) {
+//      let data = await response.text();
+//      let obj = JSON.parse(data);
+//      for (let i=0; i<obj.data.length; i++) {
+//        let counter = obj.data[i];
+//        test.push(`id:${counter.id}, first name:${counter.first_name}, last name:${counter.last_name}, email:${counter.email}, avatar:${counter.avatar}`);
+//        console.log(test);
+//        document.querySelector('#result').innerHTML = test;
+//      }
+//    }
+//    }
+//    catch (error) {
+//        console.log(error);
+//   }
+//}
 
 //const json = '{ "fruit": "pineapple", "fingers": [{"id":10}] }';
 //const obj = JSON.parse(json);
@@ -29,3 +29,62 @@ document.querySelector('#get-text').onclick = async () => {
 //    var counter = jsonData.counters[i];
 //    console.log(counter.counter_name);
 //}
+
+
+//let div = document.createElement('div');
+//div.setAttribute('class', 'myblock');
+//div.innerHTML=`<div class="myblock" id="mymy">
+//        <div class="miniblocks"><h1>Классный</h1></div>
+//            <div class="miniblocks">Красный блок</div>
+//            <div class="imageforimage" id="esenin"><img src="esenin.jfif" id='imgese'></div>
+//            <div class='white'></div>
+//            <div class='purple'></div>
+//            <div class='black'></div>
+//        </div>
+//        `;
+//
+//document.querySelector('body').appendChild(div);
+//
+//let change =document.querySelector('#mymy');
+//let textt= document.querySelector('#texte');
+//change.style.color='red';
+//let myesenin = document.querySelector('#esenin');
+//myesenin.style.width='500px';
+//let imge = document.querySelector('#imgese');
+//imge.style.width='100%';
+
+//let a = document.createElement('h1');
+//a.textContent='gfdgdfg';
+//document.querySelector('body').appendChild(a);
+//a.setAttribute('class', 'colors');
+//a.style.fontSize = '60px';
+//
+//let json = '{"id":1, "name":"Костян", "id":2, "name":"Иван"}';
+//let b = JSON.parse(json);
+//console.log(b);
+//let watch = document.querySelector('#zagol');
+//for (i=0;i<json.length;i++){
+//    watch.innerHTML=`${b.name}`;
+//}
+//const json = '{"result":true, "count":42}';
+//const obj = JSON.parse(json);
+//console.log(obj.count);
+
+
+let ab = parseInt(prompt("Введите сторону ab"))
+let bc = parseInt(prompt("Введите сторону bc"))
+let ac = parseInt(prompt("Введите сторону ac"))
+
+function treugol(ab,bc,ac){
+    if (ab === bc && bc === ac){
+        alert("Ваш треугольник равносторонний")
+    }
+    else if (ab === bc || ab === ac && ac !== bc){
+        alert("Ваш треугольник равнобедренный")
+    }
+    else{
+        alert("Ваш треугольник не равнобедренный и не равносторонний")
+    }
+}
+treugol(ab,bc,ac)
+
