b62e281de27408d1c573462affb266663e675fd7
Comp022023 <kyurinova@inbox.ru>
2023-10-07 12:02:56 +0300

    Третье задание с суммой чётных

--- js/script.js
+++ js/script.js
@@ -95,8 +95,8 @@ function execplease(){
         if( i % 2 == 0){
 
             j=j+i
+            if(j>= 13 && j<27) continue
             console.log(j)
-            if (j >= 20) break
         }
     }
     }
